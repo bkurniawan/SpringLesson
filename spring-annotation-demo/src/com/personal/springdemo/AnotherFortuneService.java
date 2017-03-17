f32c29ee21f7e46e96960307901b072b226e74d2
16052562 <16052562@NBC0BALUSITFE.corp.bankbtpn.co.id>
2017-03-17 15:55:43 +0700

    Autowired lesson

---
+++ spring-annotation-demo/src/com/personal/springdemo/AnotherFortuneService.java
@@ -0,0 +1,14 @@
+package com.personal.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnotherFortuneService implements FortuneService {
+
+	@Override
+	public String getFortuneMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
